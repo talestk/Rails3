6d0d90af8ded81cc3c6ddcdef729ec5fbe20534c
Tales Takemiya <talestk@hotmail.com>
2012-01-10 17:09:57 -0600

    Added ability to edit and update users

--- ticketee/app/controllers/admin/users_controller.rb
+++ ticketee/app/controllers/admin/users_controller.rb
@@ -1,16 +1,47 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all(:order => "email")
+  end
+  
+  def show
+    
   end
   
   def new
     @user = User.new
   end
+  
+  def update
+    # fizing the blank password that Devise don't allow
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    #making sure that the user is made an admin before sace for update_attributes
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+      
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def destroy
+    
+  end
     
   def create
     @user = User.new(params[:user])
-    # it sets the admin attribute on the user 
-    @user.admin = params[:user][:admin] = "1"
+    set_admin
     
     if @user.save
       flash[:notice] = "User has been created."
@@ -20,5 +51,15 @@ class Admin::UsersController < Admin::BaseController
       flash[:alert] = "User has not been created."
       render :action => "new"
     end 
+  end
+  
+  private
+  
+  def find_user
+    @user = User.find(params[:id])
+  end
+  
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
   end
 end
