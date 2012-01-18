f6a2b1438cb7f07eeeee1e3b87c0a4ea3d2cda3a
Tales Takemiya <talestk@hotmail.com>
2012-01-18 12:17:18 -0600

    Restrict destroy action to only people with permission

--- ticketee/features/step_definitions/permission_steps.rb
+++ ticketee/features/step_definitions/permission_steps.rb
@@ -10,3 +10,15 @@ Given /^"([^"]*)" can create tikets in the "([^"]*)" project$/ do |user, project
                      :action => "create tickets")
 end
 
+Given /^"([^"]*)" can delete tickets in the "([^"]*)" project$/ do |user, project|
+  Permission.create!(:user => User.find_by_email!(user),
+                     :thing => Project.find_by_name!(project),
+                     :action => "delete tickets")
+end
+
+Given /^"([^"]*)" can edit tickets in the "([^"]*)" project$/ do |user, project|
+  Permission.create!(:user => User.find_by_email!(user),
+                       :thing => Project.find_by_name!(project),
+                       :action => "edit tickets")
+  
+end
